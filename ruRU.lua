caf1c06f93864c1b10b4cdbd76427609f6612e3b
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-09-06 06:22:34 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,14 @@
+-- Russian(RU) Localization ZamestoTV
+
+if(GetLocale() == "ruRU")then
+	REQUEUEHeaderText                   = "Подземелье завершено"
+	REQUEUE_BUTTONTXT1                  = "Покинуть группу"
+	REQUEUE_BUTTONTXT2                  = "Сказать спасибо!"
+	REQUEUE_SAYTHANKS_INCLUDE_NAME      = "Спасибо за группу!"
+	REQUEUEConfigHeaderText             = "Меню настроек"
+	REQUEUE_ExpandDesc1                 = "Дополнительные подземелья"
+	REQUEUE_ExpandDesc2                 = "Если отмечено, пул подземелий будет автоматически расширен через 5 минут."
+	
+-- Classic Era Locale
+	REQUEUE_CLASSIC_INQUEUE             = "Ожидание в очереди..."
+end
